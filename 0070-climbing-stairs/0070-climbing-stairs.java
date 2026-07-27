3a284ab6e2cf97cf69c5010bf5c99df4f13d4231
Abdul khader 117 <shaikabdulk1976@gmail.com>
2026-07-27 16:37:31 +0530

    Time: 0 ms (100%), Space: 42.1 MB (55.07%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -3,12 +3,13 @@ class Solution {
         if(n<=1){
             return n;
         }
-        int[] arr = new int[n];
-        arr[0] = 1;
-        arr[1] = 2;
+        int f = 1;
+        int s = 2;
         for(int i=2; i<n; i++){
-            arr[i] = arr[i-1] + arr[i-2];
+            int current = f + s;
+            f = s;
+            s = current;
         }
-        return arr[n-1];
+        return s;
     }
 }
