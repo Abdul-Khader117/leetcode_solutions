b4cf7a57972979cc8960eb41988f526725b81b32
Abdul khader masthan <shaikabdulk1976@gmail.com>
2026-08-22 12:05:28 +0530

    Time: 1 ms (25.54%), Space: 42.6 MB (9.96%) - LeetHub

--- 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.java
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.java
@@ -3,15 +3,13 @@ class Solution {
         int sum = 0;
         int product = 1;
         int num=n;
-        while(n > 0){
-            sum += n % 10;
-            product *= n % 10;
-            n = n / 10;
+        while(num > 0){
+            int d=num%10;
+            sum+=d;
+            product*=d;
+            num = num / 10;
         }
-        int abdul = (sum + product);
-        if(num % abdul == 0){
-            return true;
-        }
-        return false;
+        
+        return n%(sum+product)==0;
     }
 }
